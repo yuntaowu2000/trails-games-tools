76a2846eef1f861e606c1d22a2bfe88e76950880
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2021-02-08 10:11:27 -0800

    Create extractImg.py

---
+++ extractImg.py
@@ -0,0 +1,27 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+site = 'https://steamcommunity.com/stats/538680/achievements'
+
+response = requests.get(site)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+img_tags = soup.find_all('img')
+
+urls = [img['src'] for img in img_tags]
+
+
+for url in urls:
+    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+    if not filename:
+         print("Regex didn't match with the url: {}".format(url))
+         continue
+    with open(filename.group(1), 'wb') as f:
+        if 'http' not in url:
+            # sometimes an image source can be relative 
+            # if it is provide the base url which also happens 
+            # to be the site variable atm. 
+            url = '{}{}'.format(site, url)
+        response = requests.get(url)
+        f.write(response.content)
